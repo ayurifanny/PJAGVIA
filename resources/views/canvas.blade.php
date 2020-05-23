81441d793d913f340863f4302173e78c012655c4
renitanapitupulu <renitairen1613@gmail.com>
2020-05-23 15:32:28 +0700

    wkwkwkwk

--- resources/views/canvas.blade.php
+++ resources/views/canvas.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.app')
+@section('navbar')
+@if(auth()->user()->hasRole('customer'))
+    <li class="nav-item mb-3">
+        <a class="nav-link d-inline-block" href="/home"><i class="fas fa-calendar mr-2"></i>Request Inspection</a>
+    </li>
+    <li class="nav-item mb-3">
+        <a class="nav-link d-inline-block" href="/history_meeting"><i class="fas fa-history mr-2"></i>History</a>
+    </li>
+@elseif(auth()->user()->hasRole('inspector'))
+    <li class="nav-item mb-3">
+        <a class="nav-link" href="/home"><i class="fas fa-tachometer-alt mr-2"></i>Dashboard</a>
+    </li>
+    <li class="nav-item mb-3">
+        <a class="nav-link" href="/home"><i class="fas fa-list-ul mr-2"></i>List of Inspection Request</a>
+    </li>
+    <li class="nav-item mb-3">
+        <a class="nav-link" href="/history_meeting"><i class="fas fa-history mr-2"></i>History</a>
+    </li>
+@endif
+@endsection
+
 
 @section('styles')
 <link href="{{ asset('css/canvas.css') }}" rel="stylesheet">
