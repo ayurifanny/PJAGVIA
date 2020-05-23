26de1990e4360374c97017ea8d3dcf0b87745af3
renitanapitupulu <34207372+renitanapitupulu@users.noreply.github.com>
2020-05-23 15:40:00 +0700

    Update canvas.blade.php

--- resources/views/canvas.blade.php
+++ resources/views/canvas.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+
 @section('navbar')
 @if(auth()->user()->hasRole('customer'))
     <li class="nav-item mb-3">
@@ -64,16 +65,6 @@
 @endsection
 @endif
 
-@section('navbar')
-@if(auth()->user()->hasRole('customer'))
-    <li class="nav-item">
-        <a class="nav-link" href="/home">Request Meeting</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/history_meeting">History</a>
-    </li>
-@endif
-@endsection
 
 @section('content')
 <div class="container">
