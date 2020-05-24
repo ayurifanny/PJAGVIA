69fe55dce4b7ebaad356385265733a770071c2e5
Ayu Rifanny <13517038@std.stei.itb.ac.id>
2020-05-24 23:07:05 +0700

    add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $user = App\User::create([
+            'name' => 'Admin',
+            'email' => 'admin1@example.com',
+            'password' => '$2y$10$B17Uj4DH2GruuxyLA2fjb.z79Q8pQ4oJjI6HuuQvDmT7ECOc2EXQm',
+        ]);
+
+        Role::create([
+            'name' => 'customer',
+            'guard_name' => 'web',
+        ]);
+
+        Role::create([
+            'name' => 'inspector',
+            'guard_name' => 'web',
+        ]);
+
+        Role::create([
+            'name' => 'admin',
+            'guard_name' => 'web',
+        ]);
+
+        $user->assignRole('admin');
     }
 }
